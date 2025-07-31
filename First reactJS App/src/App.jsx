17e1e90b4cb0414fc9f67fb6f9740d8831eb34e8
Aarya Shah <148608350+shahaarya465@users.noreply.github.com>
2025-07-31 11:07:35 +0530

    Update App.jsx

--- First reactJS App/src/App.jsx
+++ First reactJS App/src/App.jsx
@@ -1,45 +1,9 @@
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-// import './App.css'
-
-// function App() {
-//   const [count, setCount] = useState(0)
-
-//   return (
-//     <>
-//       <div>
-//         <a href="https://vite.dev" target="_blank">
-//           <img src={viteLogo} className="logo" alt="Vite logo" />
-//         </a>
-//         <a href="https://react.dev" target="_blank">
-//           <img src={reactLogo} className="logo react" alt="React logo" />
-//         </a>
-//       </div>
-//       <h1>Vite + React</h1>
-//       <div className="card">
-//         <button onClick={() => setCount((count) => count + 1)}>
-//           count is {count}
-//         </button>
-//         <p>
-//           Edit <code>src/App.jsx</code> and save to test HMR
-//         </p>
-//       </div>
-//       <p className="read-the-docs">
-//         Click on the Vite and React logos to learn more
-//       </p>
-//     </>
-//   )
-// }
-
-// export default App
-
-
 import Home from "./Home";
 import About from "./About";
 import Contact from "./Contact";
 import Eventdemo from "./Eventdemo";
 import Counter from "./Counter";
+import Sum from "./sumdemo";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 function App() {
@@ -57,6 +21,8 @@ function App() {
         <br />
         <Link to="/Counter">Counter</Link>
         <br />
+        <Link to="/Sum">Sum</Link>
+        <br />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/Home" element={<Home />} />
@@ -64,6 +30,7 @@ function App() {
           <Route path="/Contact" element={<Contact />} />
           <Route path="/Eventdemo" element={<Eventdemo />} />
           <Route path="/Counter" element={<Counter />} />
+          <Route path="/Sum" element={<Sum />} />
           <Route path="/Cake/Ahmedabad" element={<h1>Ahmedabad cake</h1>} />
           <Route path="/Cake/Vadodara" element={<h1>Vadodara cake</h1>} />
         </Routes>
